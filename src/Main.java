94b0e4467f9628a8d917ff1b64a040f737756614
Ksu <b1.shchedrina.oksana@gmail.com>
2023-04-04 22:58:22 +0200

    dz-4

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello dz-4!");
+
+
+    secondClass retC = new secondClass();
+        System.out.println("Returned value from secondClass is: " + secondClass.printC());
+    }
+}
---
+++ src/secondClass.java
@@ -0,0 +1,7 @@
+public class secondClass {
+    public static int printC () {
+        int c = 22;
+        System.out.println(String.format("The result is: %d",c));
+        return c;
+    }
+}
