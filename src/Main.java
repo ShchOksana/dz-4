2d1558dbbcb055d57d57d2326004f32034e266d1
Ksu <b1.shchedrina.oksana@gmail.com>
2023-04-04 23:09:13 +0200

    update dz-4

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello dz-4!");
+        System.out.println("Returned value c from secondClass is: " + secondClass.printC());
 
-
-    secondClass retC = new secondClass();
-        System.out.println("Returned value from secondClass is: " + secondClass.printC());
+        secondClass retA = new secondClass();
+        System.out.println("Returned value a from secondClass is: " + retA.a);
     }
 }
